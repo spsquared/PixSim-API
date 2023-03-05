3863bcc7b579572d4c47499b603d2c7d2c6e0e64
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2023-03-05 17:54:07 +0000

    websockets

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-console.info('Starting PixSim API');
+console.info('Starting PixSim Proxy API');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 1000,
   max: 300,
   handler: function(req, res, options) { }
 });
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}));
 app.use(limiter);
+
+app.get('/', (req, res) => res.send({ active: true }));
+
+if (process.env.PORT) {
+  server.listen(process.env.PORT);
+} else {
+  server.listen(503);
+}
 
 const { subtle } = require('crypto').webcrypto;
 const keys = subtle.generateKey({
@@ -23,36 +36,86 @@ const recentConnections = [];
 const recentConnectionKicks = [];
 const io = new (require('socket.io')).Server(server, {
   path: '/pixsim-api/',
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  },
   pingTimeout: 10000,
   upgradeTimeout: 300000
 });
 io.on('connection', function(socket) {
+  // connection DOS detection
   socket.handshake.headers['x-forwarded-for'] = socket.handshake.headers['x-forwarded-for'] ?? '127.0.0.1';
-  recentConnections[socket.handshake.headers['x-forwarded-for']] = (recentConnections[socket.handshake.headers['x-forwarded-for']] ?? 0) + 1;
-  if (recentConnections[socket.handshake.headers['x-forwarded-for']] > 3) {
-    if (!recentConnectionKicks[socket.handshake.headers['x-forwarded-for']]) log('IP ' + socket.handshake.headers['x-forwarded-for'] + ' was kicked for connection spam.');
-    recentConnectionKicks[socket.handshake.headers['x-forwarded-for']] = true;
-    socket.emit('disconnected');
+  const ip = socket.handshake.headers['x-forwarded-for'];
+  recentConnections[ip] = (recentConnections[ip] ?? 0) + 1;
+  if (recentConnections[ip] > 3) {
+    if (!recentConnectionKicks[ip]) log(ip + ' was kicked for connection spam.');
+    recentConnectionKicks[ip] = true;
+    socket.emit('disconnection: ' + ip);
     socket.removeAllListeners();
     socket.onevent = function(packet) { };
     socket.disconnect();
     return;
   }
+  console.log('connection: ' + ip);
   // public RSA key
   socket.once('requestPublicKey', async function() {
     socket.emit('publicKey', await subtle.exportKey('jwk', (await keys).publicKey));
   });
 
   // manage disconnections
-  const disconnectPings = setInterval(function() {
-    socket.emit('ping');
-    let timeoutdetect = setTimeout(async function() {
-      await player.leave();
-      clearInterval(disconnectPings);
-    }, 10000);
-    socket.once('pong', function() {
-      clearTimeout(timeoutdetect);
-    });
+  socket.on('disconnect', async function() {
+    socket.emit('disconnection: ' + ip);
+    clearInterval(timeoutcheck);
+    clearInterval(packetcheck);
+  });
+  socket.on('disconnected', async function() {
+    socket.emit('disconnection: ' + ip);
+    clearInterval(timeoutcheck);
+    clearInterval(packetcheck);
+  });
+  socket.on('timeout', async function() {
+    socket.emit('disconnection: ' + ip);
+    clearInterval(timeoutcheck);
+    clearInterval(packetcheck);
+  });
+  socket.on('error', async function() {
+    socket.emit('disconnection: ' + ip);
+    clearInterval(timeoutcheck);
+    clearInterval(packetcheck);
+  });
+  // timeout
+  let timeout = 0;
+  const timeoutcheck = setInterval(async function() {
+    timeout++;
+    if (timeout > 300) {
+      clearInterval(timeoutcheck);
+      clearInterval(packetcheck);
+    }
+  }, 1000);
+  // performance metrics
+  socket.on('ping', function() {
+    socket.emit('pong');
+  });
+  // dos spam protection
+  let packetCount = 0;
+  const onevent = socket.onevent;
+  socket.onevent = function(packet) {
+    if (packet.data[0] == null) {
+      socket.disconnect();
+    }
+    onevent.call(this, packet);
+    timeout = 0;
+    packetCount++;
+  };
+  const packetcheck = setInterval(async function() {
+    packetCount = Math.max(packetCount - 250, 0);
+    if (packetCount > 0) {
+      clearInterval(timeoutcheck);
+      clearInterval(packetcheck);
+      console.log(ip + ' was kicked for packet spam');
+      socket.disconnect();
+    }
   }, 1000);
 });
 setInterval(function() {
@@ -63,6 +126,15 @@ setInterval(function() {
     delete recentConnectionKicks[i];
   }
 }, 1000);
+
+function stop() {
+  io.emit('disconnceted')
+  io.close();
+};
+process.on('SIGTERM', stop);
+process.on('SIGINT', stop);
+process.on('SIGQUIT', stop);
+process.on('SIGILL', stop);
 
 async function RSAdecodeAPI(buf) {
   return new TextDecoder().decode(await subtle.decrypt({ name: "RSA-OAEP" }, (await keys).privateKey, buf));
---
+++ rooms.js
@@ -0,0 +1,8 @@
+Room = function(host) {
+  // stores the type of game room, a list of client sockets, and a host socket
+  this.type = 'none';
+  this.clients = [];
+  this.host = host;
+
+  // todo - forwarding stuff
+};
